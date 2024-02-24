ac398bca636ea2de4c01a72757683f580ef220bf
GevaX <gevax1010@gmail.com>
2024-02-24 17:36:10 +0200

    24/2/24 17:35

--- mapp.js
+++
@@ -1,9 +0,0 @@
-setTimeout(() => {
-  const page = document.getElementById('page');
-
-  // 👇️ removes element from DOM
-  box.style.display = 'none';
-
-  // 👇️ hides element (still takes up space on page)
-  // box.style.visibility = 'hidden';
-}, 1000); // 👈️ time in milliseconds
